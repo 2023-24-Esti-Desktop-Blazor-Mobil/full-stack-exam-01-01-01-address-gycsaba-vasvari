9699eebaca3a5b7a911c564b4a2aa5546ba7a232
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-19 18:13:04 +0100

    ToDto és ToModel metódusok

---
+++ Kreta.Shared/Extensions/AddressExtension.cs
@@ -0,0 +1,13 @@
+﻿using Kreta.Shared.Dtos;
+using Kreta.Shared.Models;
+
+namespace Kreta.Shared.Extensions
+{
+    public class AddressExtension
+    {
+        public AddressDto ToDto()
+        { }
+
+        public Address ToModel() { }
+    }
+}
