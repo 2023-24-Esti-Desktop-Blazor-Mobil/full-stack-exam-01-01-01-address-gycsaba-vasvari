c212ade86989585db63cf770d433f95a459d1eb1
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-19 19:16:15 +0100

    Addresses DbSet megadása

--- Kreta.Backend/Context/KretaContext.cs
+++ Kreta.Backend/Context/KretaContext.cs
@@ -1,4 +1,5 @@
-﻿using Kreta.Shared.Models.SchoolCitizens;
+﻿using Kreta.Shared.Models;
+using Kreta.Shared.Models.SchoolCitizens;
 using Microsoft.EntityFrameworkCore;
 
 namespace Kreta.Backend.Context
@@ -6,6 +7,7 @@ namespace Kreta.Backend.Context
     public class KretaContext : DbContext
     {
         public DbSet<Student> Students { get; set; }
+        public DbSet<Address> Addresses { get; set; }
         public KretaContext(DbContextOptions options) : base(options)
         {
         }
