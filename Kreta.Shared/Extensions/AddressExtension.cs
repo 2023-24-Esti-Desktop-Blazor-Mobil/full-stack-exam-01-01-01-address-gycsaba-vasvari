7086ab434307372fc3e1320d325136e1ed6433c7
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-19 18:19:32 +0100

    A konstruktor hívás a metódusokban

--- Kreta.Shared/Extensions/AddressExtension.cs
+++ Kreta.Shared/Extensions/AddressExtension.cs
@@ -6,8 +6,21 @@ namespace Kreta.Shared.Extensions
     public static class AddressExtension
     {
         public static AddressDto ToDto(this Address address)
-        { }
+        {
+            return new AddressDto
+            {
+                ZipCode = address.ZipCode,
+                Street = address.Street,
+            };
+        }
 
-        public static Address ToModel(this AddressDto addressDto) { }
+        public static Address ToModel(this AddressDto addressDto) 
+        {
+            return new Address
+            {
+                ZipCode = addressDto.ZipCode,
+                Street = addressDto.Street,
+            };
+        }
     }
 }
