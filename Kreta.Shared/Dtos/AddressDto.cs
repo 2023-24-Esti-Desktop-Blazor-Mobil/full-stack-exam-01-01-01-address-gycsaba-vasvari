9ccd36db225bf19bf0ecdc3fc2674a9fdc73edfd
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-19 18:08:13 +0100

    Address és AddressDto

---
+++ Kreta.Shared/Dtos/AddressDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreta.Shared.Dtos
+{
+    public class AddressDto
+    {
+        public int ZipCode { get; set; }
+        public string Street { get; set; } = string.Empty;
+    }
+}
---
+++ Kreta.Shared/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreta.Shared.Models
+{
+    public class Address
+    {
+        public int ZipCode { get; set; }
+        public string Street { get; set; } = string.Empty;
+
+        public bool IsValidZipCode => ZipCode >0;
+        public bool IsStreetValid => ! string.IsNullOrEmpty(Street);
+    }
+}
