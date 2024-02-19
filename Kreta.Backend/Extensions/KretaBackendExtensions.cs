3003237f5c9c90c8b37dc1d7a8d35ecc40a7f01b
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-19 19:11:31 +0100

    IAddressRepo servicként éled fel

--- Kreta.Backend/Extensions/KretaBackendExtensions.cs
+++ Kreta.Backend/Extensions/KretaBackendExtensions.cs
@@ -40,7 +40,7 @@ namespace Kreta.Backend.Extensions
             services.AddScoped<IParentRepo, ParentInMemoryRepo>();
             services.AddScoped<ITeacherRepo, TeacherInMemoryRepo>();
             services.AddScoped<ISubjectRepo, SubjectInMemoryRepo>();
-            services.AddScoped<IAddressRepo, >
+            services.AddScoped<IAddressRepo, AddressInMemoryRepo>();
         }
 
         public static void ConfigureAssamblers(this IServiceCollection services)
