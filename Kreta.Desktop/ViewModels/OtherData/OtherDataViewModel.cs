8970205d8907cd1b44155f6732f25035f2a70af1
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-21 17:45:47 +0100

    CurrentChildView és ShowAddressViewCommand

--- Kreta.Desktop/ViewModels/OtherData/OtherDataViewModel.cs
+++ Kreta.Desktop/ViewModels/OtherData/OtherDataViewModel.cs
@@ -1,10 +1,31 @@
-﻿using Kreta.Desktop.ViewModels.Base;
+﻿using CommunityToolkit.Mvvm.Input;
+using FontAwesome.Sharp;
+using Kreta.Desktop.ViewModels.Base;
 
 
 namespace Kreta.Desktop.ViewModels.OtherData
 {
-    public class OtherDataViewModel : BaseViewModel
+    public partial class OtherDataViewModel : BaseViewModel
     {
+        private AddressViewModel _addressViewModel;
         public BaseViewModel CurrentChildView { get; set; }
+
+        public OtherDataViewModel()
+        {
+            _addressViewModel=new AddressViewModel();
+            CurrentChildView = new AddressViewModel();
+        }
+
+        public OtherDataViewModel(AddressViewModel addressViewModel)
+        {
+            _addressViewModel = addressViewModel;
+            CurrentChildView = _addressViewModel;
+        }
+
+        [RelayCommand]
+        private void ShowAddressView()
+        {            
+            CurrentChildView = _addressViewModel;
+        }
     }
 }
