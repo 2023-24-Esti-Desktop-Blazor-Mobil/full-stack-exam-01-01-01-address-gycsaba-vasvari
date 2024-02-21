1924f465778d7286ea1d051a78bc7d7f94b3f9ca
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-21 16:21:52 +0100

    AddressService konstruktorok

--- Kreta.HttpService/Service/AddressService.cs
+++ Kreta.HttpService/Service/AddressService.cs
@@ -1,6 +1,42 @@
-﻿namespace Kreta.HttpService.Service
+﻿using Kreta.Shared.Models;
+using Kreta.Shared.Responses;
+
+namespace Kreta.HttpService.Service
 {
     public class AddressService : IAddressService
     {
+        private readonly HttpClient? _httpClient;
+
+        public AddressService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public AddressService(IHttpClientFactory? httpClientFactory) 
+        {
+            if (httpClientFactory is not null)
+            {
+                _httpClient = httpClientFactory.CreateClient("KretaApi");
+            }
+        }
+        public Task<ControllerResponse> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ControllerResponse> InsertAsync(Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Address>> SelectAllAddressAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ControllerResponse> UpdateAsync(Address address)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
