dd57a0f16acaa6c1bbbdf3724bedcd10719409a4
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-21 16:55:49 +0100

    Addresses változó a címek tárolására

--- Kreta.Desktop/ViewModels/OtherData/AddressViewModel.cs
+++ Kreta.Desktop/ViewModels/OtherData/AddressViewModel.cs
@@ -1,14 +1,21 @@
-﻿using Kreta.HttpService.Service;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Kreta.Desktop.ViewModels.Base;
+using Kreta.HttpService.Service;
+using Kreta.Shared.Models;
+using System.Collections.ObjectModel;
 
 namespace Kreta.Desktop.ViewModels.OtherData
 {
-    public class AddressViewModel
+    public partial class AddressViewModel : BaseViewModel
     {
         private readonly IAddressService _addressService;
 
+        [ObservableProperty]
+        public ObservableCollection<Address> _addresses;
         public AddressViewModel()
         {
             _addressService = new AddressService();
+            _addresses = new ObservableCollection<Address>();
         }
 
         public AddressViewModel(IAddressService addressService)
