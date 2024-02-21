b4088a762cf2fb990686aa87eccc5ecbc548c2cf
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-21 16:26:49 +0100

    SelectAllAddressAsync metódus

--- Kreta.HttpService/Service/AddressService.cs
+++ Kreta.HttpService/Service/AddressService.cs
@@ -1,5 +1,10 @@
-﻿using Kreta.Shared.Models;
+﻿using Kreta.Shared.Dtos;
+using Kreta.Shared.Extensions;
+using Kreta.Shared.Models;
+using Kreta.Shared.Models.SchoolCitizens;
 using Kreta.Shared.Responses;
+using System.Diagnostics;
+using System.Net.Http.Json;
 
 namespace Kreta.HttpService.Service
 {
@@ -10,6 +15,27 @@ namespace Kreta.HttpService.Service
         public AddressService()
         {
             _httpClient = new HttpClient();
+        }
+
+        public async Task<List<Address>> SelectAllAddressAsync()
+        {
+            if (_httpClient is not null)
+            {
+                try
+                {
+                    List<AddressDto>? resultDto = await _httpClient.GetFromJsonAsync<List<AddressDto>>("api/Address");
+                    if (resultDto is not null)
+                    {
+                        List<Address> result = resultDto.Select(addressDto => addressDto.ToModel()).ToList();
+                        return result;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+            return new List<Address>();
         }
 
         public AddressService(IHttpClientFactory? httpClientFactory) 
@@ -27,12 +53,7 @@ namespace Kreta.HttpService.Service
         public Task<ControllerResponse> InsertAsync(Address address)
         {
             throw new NotImplementedException();
-        }
-
-        public Task<List<Address>> SelectAllAddressAsync()
-        {
-            throw new NotImplementedException();
-        }
+        }    
 
         public Task<ControllerResponse> UpdateAsync(Address address)
         {
